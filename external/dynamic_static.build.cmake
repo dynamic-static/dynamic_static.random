850788fa964f40aa646afbdc8edede4e877a9f17
PatrickPurcell <patrick.r.purcell@gmail.com>
2022-02-05 21:05:21 -0800

    Revert "Updating commit hashes"
    This reverts commit e006edcfe84165c5b7afea148a0d1ad3d8b6b786.

--- external/dynamic_static.build.cmake
+++ external/dynamic_static.build.cmake
@@ -1,2 +1,22 @@
-# ==========================================#   Copyright (c) 2020-2021 dynamic_static#       Licensed under the MIT license#     http://opensource.org/licenses/MIT# ==========================================include(FetchContent)if(EXISTS "${DYNAMIC_STATIC}/dynamic_static.build/")    FetchContent_Declare(dynamic_static.build SOURCE_DIR "${DYNAMIC_STATIC}/dynamic_static.build/")else()    FetchContent_Declare(        dynamic_static.build        GIT_REPOSITORY "https://github.com/dynamic-static/dynamic_static.build.git"        GIT_TAG d43db5c7e0788b1e34883aa5fdaaf24a8435e5d9
-        GIT_PROGRESS TRUE        FETCHCONTENT_UPDATES_DISCONNECTED    )endif()FetchContent_MakeAvailable(dynamic_static.build)include("${dynamic_static.build_SOURCE_DIR}/dynamic_static.build.cmake")
+
+# ==========================================
+#   Copyright (c) 2020-2021 dynamic_static
+#       Licensed under the MIT license
+#     http://opensource.org/licenses/MIT
+# ==========================================
+
+include(FetchContent)
+
+if(EXISTS "${DYNAMIC_STATIC}/dynamic_static.build/")
+    FetchContent_Declare(dynamic_static.build SOURCE_DIR "${DYNAMIC_STATIC}/dynamic_static.build/")
+else()
+    FetchContent_Declare(
+        dynamic_static.build
+        GIT_REPOSITORY "https://github.com/dynamic-static/dynamic_static.build.git"
+        GIT_TAG c67ee23b538880cb2027dd1e73dad41a8071bbce
+        GIT_PROGRESS TRUE
+        FETCHCONTENT_UPDATES_DISCONNECTED
+    )
+endif()
+FetchContent_MakeAvailable(dynamic_static.build)
+include("${dynamic_static.build_SOURCE_DIR}/dynamic_static.build.cmake")
